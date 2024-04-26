88ae8f350b42202953be27309b56c164833dd67a
Eric <ethai1@cps.edu>
2024-04-26 11:27:23 -0500

    worked on neural net

--- abalone_neural_net.py
+++ abalone_neural_net.py
@@ -3,4 +3,17 @@ from sklearn.model_selection import train_test_split
 from neural_net_UCI_data import parse_line, normalize
 
 with open("abalone_data.txt", "r") as file:
-    training_data = [parse_line(line) for line in file.readlines() if len(line) > 4]
+    training_data = [parse_line(line.strip()) for line in file.readlines() if len(line) > 4]
+
+training_data = normalize(training_data)
+
+train_data, test_data = train_test_split(training_data)
+
+# print(train_data)
+
+network = NeuralNet(8, 3, 1)
+network.train(train_data)
+
+for i in network.test_with_expected(test_data):
+    difference = round(abs(i[1][0] - i[2][0]), 3)
+    print(f"desired: {i[1]}, actual: {i[2]} diff: {difference}")
